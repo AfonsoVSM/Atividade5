d8d7a8349e6634766783fc4a49867d0114fcc442
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-15 12:26:40 +0100

    Update dois.php, ssds.php, and 2 more files...

---
+++ tres.php
@@ -0,0 +1,15 @@
+<?php
+$txt1 = "Eu sou o Joao e";
+$txt2 = "na turma sou o numero";
+$txt3 = "Vou fazer este modulo com";
+$Num1 = "15";
+$Num2 = "20";
+
+$txt4 = "Eu sou o Afonso e";
+$txt5 = "na turma sou o numero";
+$txt6 = "Vou fazer o modulo com";
+$Num3 = "20";
+$Num4 = "20";
+echo 
+
+?>
