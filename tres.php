5a2eb5ab9e1cd75058643622163961607c9b96a4
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 11:49:57 +0100

    Update tres.php

--- tres.php
+++ tres.php
@@ -4,12 +4,37 @@ $txt2 = "na turma sou o numero";
 $txt3 = "Vou fazer este modulo com";
 $Num1 = "15";
 $Num2 = "20";
+echo $txt1. " " .$txt2. " " .$Num1. ". " .$txt3. " " .$Num2. " Valores.";
+echo "<br>";
+echo "<br>";  
 
-$txt4 = "Eu sou o Afonso e";
-$txt5 = "na turma sou o numero";
-$txt6 = "Vou fazer o modulo com";
-$Num3 = "20";
-$Num4 = "20";
-echo 
+$txt1 = "Eu sou o Afonso e";
+$txt2 = "na turma sou o numero";
+$txt3 = "Vou fazer o modulo com";
+$Num1 = "1";
+$Num2 = "20";
+echo $txt1. " " .$txt2. " " .$Num1. ". " .$txt3. " " .$Num2. " Valores.";
+
+echo "<br>";
+echo "<br>";    
+print "Os testes estão no nosso disco de rede em \\Curso\Disciplinas\Modulos\Testes\"";
+
+echo "<br>";
+echo "<br>";    
+print "Em PHP, a declaração de uma variável implica a precedência de um carácter especial: o carater $.";  
+
+echo "<br>";
+echo "<br>";    
+print "O professor chegou perto do aluno e exclamou: “se não estudares, vais reprovar de ano!”.";  
+
+echo"<br>";
+echo"<br>";
+$txt1="O professor";
+$txt2="chegou perto";
+$txt3="do aluno e";
+$txt4="exclamou: “se não";
+$txt5="estudares, vais";
+$txt6="reprovar de ano!”.";
+echo $txt1. "<br>" .$txt2. "<br>" .$txt3."<br>" .$txt4. "<br>" .$txt5. "<br>" .$txt6;
 
 ?>
